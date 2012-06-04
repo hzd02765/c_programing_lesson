1a762d7fd9a27856bda3f5cb357eb908c8dc0426
SakaiTomoyuki@MacBook <hzd02765@nifty.com>
2012-06-05 06:19:27 +0900

    page 95

---
+++ list_04/04-03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 128
+
+int main(void);
+
+int main(void)
+{
+  char buf[MAX_LINE];
+  int n;
+
+  printf("降水確率を入力してください。\n");
+  gets(buf);
+  n = atoi(buf);
+  printf("降水確率は　%d　%%です。\n", n);
+
+  if(n < 0 || 100 < n){
+    printf("降水確率は０〜１００の間ですよ。\n");
+  }else if(n >= 50){
+    printf("傘を忘れずにね。\n");
+  }else{
+    printf("傘はいりません。\n");
+  }
+
+  printf("いってらっしゃい。\n");
+
+  return (0);
+}
